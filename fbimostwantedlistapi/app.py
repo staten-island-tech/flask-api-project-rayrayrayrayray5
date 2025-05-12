e01e837ef4e66128d230411440435d763d341115
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-05-12 09:23:15 -0400

    daftregrghgrhhhgh

--- fbimostwantedlistapi/app.py
+++ fbimostwantedlistapi/app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    response = requests.get('https://api.fbi.gov/wanted/v1/list')
+    response = requests.get('https://api.fbi.gov/wanted/v1/list', params={'page':current_page})
     data = json.loads(response.content)
-    total = (data['total'])
+    total = data['total']
+    current_page = data['page']
     title = data['items'][0]['title']
-    return render_template('index.html', total=total, title=title)
+    return render_template('index.html', total=total, page=current_page, title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
