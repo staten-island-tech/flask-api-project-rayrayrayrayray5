6998f1ecc15cf926eb9e4d2ea887567940f0a85e
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-05-12 09:06:38 -0400

    w3refsdv

--- fbimostwantedlistapi/app.py
+++ fbimostwantedlistapi/app.py
@@ -1,7 +1,16 @@
 import requests
 import json
+from flask import Flask, render_template
 
-response = requests.get('https://api.fbi.gov/wanted/v1/list')
-data = json.loads(response.content)
-print(data['total'])
-print(data['items'][0]['title'])
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    response = requests.get('https://api.fbi.gov/wanted/v1/list')
+    data = json.loads(response.content)
+    total = (data['total'])
+    title = data['items'][0]['title']
+    return render_template('index.html', total=total, title=title)
+
+if __name__ == '__main__':
+    app.run(debug=True)
