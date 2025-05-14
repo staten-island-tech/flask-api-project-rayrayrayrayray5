695ee8a5d6d303b056d6d46b1e50c35f3a9f0ef6
rayrayrayrayray5 <raymondl187@nycstudents.net>
2025-05-14 09:23:57 -0400

    fbubjfjbfb

--- fbimostwantedlistapi/app.py
+++ fbimostwantedlistapi/app.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     current_page = request.args.get('page', 1, type=int)
-    search = request.args.get('search', '', type=str)
+    name = request.args.get('name', '', type=str)
+    crime = request.args.get('crime', '', type = str)
 
     params = {'page': current_page}
-    if search:
-        params['title'] = search
+    if name:
+        params['title'] = name
     response = requests.get('https://api.fbi.gov/wanted/v1/list', params = params)
     data = response.json()
 
@@ -19,6 +20,9 @@ def index():
     total = data.get('total', 0)
     current_page = data.get('page', current_page)
 
-    return render_template('index.html', items=items, total=total, page=current_page, search=search)
+    if crime:
+         items = [item for item in items if crime.lower() in str(item.get('caution', '')).lower()]
+
+    return render_template('index.html', items=items, total=total, page=current_page, name = name, crime = crime)
 if __name__ == '__main__':
     app.run(debug=True)
